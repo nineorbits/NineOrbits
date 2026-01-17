f4d39f80e92dcaca350dad56dae9b923889eace6
nineorbits <nineorbits9@gmail.com>
2026-01-17 23:06:50 +0500

    refactor: Remove React app and adopt static HTML
    Replaces the entire React application with a static HTML file. This simplifies the build process and is suitable for the current deployment target. Updates dependencies and CSS for the static site.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,3 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+console.log("NineOrbits Static Website Loaded Successfully");
+// This file can be deleted if only using index.html on GitHub Pages
